39e5b09551106b516fc451d94e950787eea62594
ethanmidgley <ethan.c.midgley@gmail.com>
2026-06-30 22:23:13 +0100

    feat: add whitespace highlighting to nvim

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -31,6 +31,32 @@ return {
   },
 
   {
+    "johnfrankmorgan/whitespace.nvim",
+    opts = {
+      -- `highlight` configures which highlight is used to display
+      -- trailing whitespace
+      highlight = "NvimInternalError",
+
+      -- `ignored_filetypes` configures which filetypes to ignore when
+      -- displaying trailing whitespace
+      ignored_filetypes = { "TelescopePrompt", "Trouble", "help", "dashboard" },
+
+      -- `ignore_terminal` configures whether to ignore terminal buffers
+      ignore_terminal = true,
+
+      -- `return_cursor` configures if cursor should return to previous
+      -- position after trimming whitespace
+      return_cursor = true,
+    },
+    config = function(_, opts)
+      local whitespaces_nvim = require("whitespace-nvim")
+      whitespaces_nvim.setup(opts)
+
+      vim.keymap.set("n", "<Leader>t", whitespaces_nvim.trim, { desc = "Trim trailing whitespace" })
+    end,
+  },
+
+  {
     "arnamak/stay-centered.nvim",
     lazy = false,
     opts = {
